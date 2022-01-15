d20df62cc14af22adbaaf494057fbd740097eb30
Deepak Patidar <developer.deepak1998@gmail.com>
2022-01-15 15:52:34 +0530

    logger removed

--- snv/settings/base.py
+++ snv/settings/base.py
@@ -153,48 +153,7 @@ REST_FRAMEWORK = {
 }
 
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": "%(asctime)s %(levelname)s %(filename)s:%(funcName)s:%(lineno)d  %(message)s"
-        },
-        "simple": {"format": "%(levelname)s %(message)s"},
-    },
-    "handlers": {
-        "app": {
-            "level": "DEBUG",
-            "class": "logging.handlers.TimedRotatingFileHandler",
-            "formatter": "verbose",
-            "filename": os.path.join(BASE_DIR, "logs", "app.log"),
-            "when": "midnight",
-            "interval": 1,
-            "backupCount": 10,
-        },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-        },
-        "mail_admins": {
-            "level": "DEBUG",
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-    },
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins", "console"],
-            "level": "DEBUG",
-            "propagate": False,
-        },
-        "app": {
-            "handlers": ["app"],
-            "level": "DEBUG",
-            "propagate": False,
-        },
-    },
-}
+
 
 
 SWAGGER_SETTINGS = {
